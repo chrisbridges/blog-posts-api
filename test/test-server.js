e6f4abe7bb7124d2ed51f4ea3b0d9ea0abe19718
chris.bridges <christopher_bridges@icloud.com>
2018-03-11 18:06:24 -0700

    added travis file

--- test/test-server.js
+++ test/test-server.js
@@ -14,5 +14,23 @@ describe('Blog Posts', function () {
     return closeServer();
   });
 
+  it('should list items on GET', function () {
+    return chai.request(app)
+    .get('/blog-posts')
+    .then(function(res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('array');
+      expect(res.body.length).to.be.at.least(1);
+
+      const expectedKeys = ['title', 'content', 'author', 'publishDate'];
+      res.body.forEach(function(item) {
+        expect(item).to.be.a('object');
+        expect(item).to.include.keys(expectedKeys);
+      });
+    });
+  });
+
+  
   
 });
