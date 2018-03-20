d06c86f97ed229a0705938fb283d11b71311d266
chris.bridges <christopher_bridges@icloud.com>
2018-03-20 11:43:25 -0700

    delete endpoint

--- server.js
+++ server.js
@@ -67,6 +67,26 @@ app.use('*', function (req, res) {
   res.status(404).json({message: 'Not found'});
 });
 
+app.delete('/posts/:id', (req, res) => {
+  BlogPost.findByIdAndRemove(req.params.id)
+  .then(() => {
+    res.status(204).json({message: 'Document successfully deleted'});
+  })
+  .catch(err => {
+    console.error(err);
+    res.json(500).json({message: 'Internal Server Error'});
+  })
+});
+
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  
+});
 
 let server;
 
